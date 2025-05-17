f9303fab557ba0e9bcda76b1c24fc0987ba42ff4
faizsarwar <faizsarwar44@gmail.com>
2025-05-18 01:12:27 +0500

    updated submission logic

--- substrate_fetcher/current_epoch_check.py
+++ substrate_fetcher/current_epoch_check.py
@@ -766,9 +766,10 @@ async def monitor_validator_epochs(pool):
             else:
                 await perform_action(current_block_number)
                 # Check if we're 5 blocks before the epoch end (block_number % 100 == 94)
-                if current_block_number == (target_block_number - 50) :
-                    await update_pin_and_storage_requests_near_epoch_end(current_block_number)
-                    await update_miner_profiles_near_epoch_end(current_block_number)
+                if current_block_number > (target_block_number - 50) :
+                    if current_block_number % 5 == 0 :
+                        await update_pin_and_storage_requests_near_epoch_end(current_block_number)
+                        await update_miner_profiles_near_epoch_end(current_block_number)
             await asyncio.sleep(5)
             continue
 
