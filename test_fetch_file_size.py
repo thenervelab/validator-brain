52b520c94f175486defa073bc1a3be45dade23cc
radu.mutilica <radu.mutilica@blackcurve.com>
2025-07-22 20:20:15 +0300

    Deleted misc scripts

--- test_fetch_file_size.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-
-from rabbitmq.pinning_request_consumer import fetch_ipfs_file_size
-
-cid = "bafkreiacxv3dy4oc7uf6v7tk7dtr5zei4u72wlg2l5bpla5yaczcgbhoma"
-
-if __name__ == "__main__":
-    asyncio.run(fetch_ipfs_file_size(cid))
