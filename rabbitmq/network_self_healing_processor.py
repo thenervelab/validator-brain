63d1fcb8ba263028382beba7082b964c594f79a3
radu.mutilica <radu.mutilica@blackcurve.com>
2025-08-07 10:24:41 +0300

    Changed batch size

--- rabbitmq/network_self_healing_processor.py
+++ rabbitmq/network_self_healing_processor.py
@@ -88,7 +88,7 @@ async def batch_submit(
     substrate,
     keypair,
     node_ids,
-    batch_size=1,
+    batch_size=10,
 ):
     """Submit deregistration reports in batches."""
     successful_batches = 0
